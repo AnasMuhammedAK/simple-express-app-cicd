74d40e50acac99107fdaddb9d1d05d2530bd2623
AnasMuhammedAK <anasabdulkareem100@gmail.comom>
2025-11-12 23:48:33 +0530

    port changed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("🚀 Hello from Express + Docker + EC2 CI/CD!");
